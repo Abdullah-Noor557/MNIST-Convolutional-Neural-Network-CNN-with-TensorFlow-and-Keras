889385e799cb435229d340f20d74c09fe47ecd0d
Abdullah-Noor557 <83589020+Abdullah-Noor557@users.noreply.github.com>
2024-05-10 04:27:25 +0500

    Create main.py
    load the mnist dataset

---
+++ main.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models, datasets
+
+def load_data():
+    (train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
+    train_images = train_images.reshape((60000, 28, 28, 1)).astype('float32') / 255
+    test_images = test_images.reshape((10000, 28, 28, 1)).astype('float32') / 255
+    return (train_images, train_labels), (test_images, test_labels)
